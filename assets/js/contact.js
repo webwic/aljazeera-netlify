c95058169b748734ef67c6ea660593643b0b99fc
mohammed ziyad <mohammedziyad@Ziyads-MacBook-Air.local>
2023-11-19 13:36:32 +0530

    contact fix

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,50 +1,53 @@
 // contact.js
 
+// Select the contact form by its ID
+const contactForm = document.querySelector('#contactForm');
 
-    const contactForm = document.querySelector('#contactForm');
-  
-    if (contactForm) {
-        contactForm.addEventListener('submit', function (event) {
-          event.preventDefault();
-      
-          const name = contactForm.querySelector('input[name="name"]').value;
-          const number = contactForm.querySelector('input[name="number"]').value;
-          const email = contactForm.querySelector('input[name="email"]').value;
-          const msg = contactForm.querySelector('input[name="msg"]').value;
-      
-          const data = {
-            name,
-            number,
-            email,
-            msg
-          };
-  
-        // You can use the fetch API or any other method to send the data to the server
-        // For simplicity, I'll use the fetch API in this example
-        fetch('/.netlify/functions/contact', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data)
-        })
-          .then(response => response.json())
-          .then(result => {
-            // Handle the contact form submission response here
-            console.log(result);
-  
-            // You can update the UI or show a confirmation message based on the response
-            if (result && result.message === 'Message sent successfully') {
-              alert('Your message has been sent successfully!');
-            } else {
-              alert('Something went wrong. Please try again.');
-            }
-          })
-          .catch(error => {
-            console.error('Error:', error);
-            alert('Something went wrong. Please try again.');
-          });
+// Check if the contact form exists on the page
+if (contactForm) {
+  // Add a submit event listener to the contact form
+  contactForm.addEventListener('submit', function (event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Get form input values
+    const name = contactForm.querySelector('input[name="name"]').value;
+    const number = contactForm.querySelector('input[name="number"]').value;
+    const email = contactForm.querySelector('input[name="email"]').value;
+    const msg = contactForm.querySelector('input[name="msg"]').value;
+
+    // Create a data object with the form values
+    const data = {
+      name,
+      number,
+      email,
+      msg
+    };
+
+    // Use the fetch API to send the data to the serverless function
+    fetch('/.netlify/functions/contact', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(result => {
+        // Handle the contact form submission response here
+        console.log(result);
+
+        // Update the UI or show a confirmation message based on the response
+        if (result && result.message === 'Message sent successfully') {
+          alert('Your message has been sent successfully!');
+        } else {
+          alert('Something went wrong. Please try again.');
+        }
+      })
+      .catch(error => {
+        // Handle errors and show an alert message
+        console.error('Error:', error);
+        alert('Something went wrong. Please try again.');
       });
-    }
-
-  
+  });
+}
--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -1,56 +1,62 @@
-
-// Function for handling contact form submissions
+// Import the axios library for making HTTP requests
 const axios = require('axios');
 
+// Define the Lambda function for handling contact form submissions
 exports.handlerContact = async function (event, context) {
-    try {
-      const { name, number, email, msg } = JSON.parse(event.body);
-  
-      const data = {
-        "messaging_product": "whatsapp",
-        "to": "971504308877",
-        "type": "template",
-        "template": {
-          "name": "bookaljazeera_complaint",
-          "language": {
-            "code": "en_US"
-          },
-          "components": [
-            {
-              "type": "body",
-              "parameters": [
-                { "type": "text", "text": name },
-                { "type": "text", "text": msg },
-                { "type": "text", "text": number },
-                { "type": "text", "text": email }
-              ]
-            }
-          ]
-        }
-      };
-  
-      const config = {
-        method: 'post',
-        url: 'https://graph.facebook.com/v14.0/113129291576893/messages',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer EAAGPYZBl29wgBAPY5ZC38fPeuBQGEJtsr5goKkNIxvhkaUz0OUvgxDEDeSYMuzA1fYmHTLliV9HM6dgqZArgpk53aL2i6x7Abqjue1GMzGQbo9pOtgOgbNuqWw2n6USNY8I0P5bWAAhyLRy0sq5ALF1jFCkIDxNp7mi3TMPdRuAnmyEnF05'
+  try {
+    // Parse incoming JSON data from the event body
+    const { name, number, email, msg } = JSON.parse(event.body);
+
+    // Prepare data for the Facebook Messenger API request
+    const data = {
+      "messaging_product": "whatsapp",
+      "to": "971504308877",
+      "type": "template",
+      "template": {
+        "name": "bookaljazeera_complaint",
+        "language": {
+          "code": "en_US"
         },
-        data: JSON.stringify(data)
-      };
-  
-      const response = await axios(config);
-      console.log(JSON.stringify(response.data));
-  
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Message sent successfully' })
-      };
-    } catch (error) {
-      console.error(error);
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: 'Something went wrong. Please try again.' })
-      };
-    }
-  };
+        "components": [
+          {
+            "type": "body",
+            "parameters": [
+              { "type": "text", "text": name },
+              { "type": "text", "text": msg },
+              { "type": "text", "text": number },
+              { "type": "text", "text": email }
+            ]
+          }
+        ]
+      }
+    };
+
+    // Configure the axios request
+    const config = {
+      method: 'post',
+      url: 'https://graph.facebook.com/v14.0/113129291576893/messages',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer EAAGPYZBl29wgBAPY5ZC38fPeuBQGEJtsr5goKkNIxvhkaUz0OUvgxDEDeSYMuzA1fYmHTLliV9HM6dgqZArgpk53aL2i6x7Abqjue1GMzGQbo9pOtgOgbNuqWw2n6USNY8I0P5bWAAhyLRy0sq5ALF1jFCkIDxNp7mi3TMPdRuAnmyEnF05'
+      },
+      data: JSON.stringify(data)
+    };
+
+    // Make the Facebook Messenger API request
+    const response = await axios(config);
+    console.log(JSON.stringify(response.data));
+
+    // Return a success response
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Message sent successfully' })
+    };
+  } catch (error) {
+    // Handle errors and return an error response
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Something went wrong. Please try again.' })
+    };
+  }
+};
